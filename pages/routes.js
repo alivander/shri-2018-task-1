292fd41673d23654b4a6dbf43bf0cda086123db6
Alina Vanieva <vanieva.alina@yandex.ru>
2018-01-14 02:14:00 +0400

    Удаляем файл controllers.js

--- pages/routes.js
+++ pages/routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
 const router = express.Router();
-const { index } = require('./controllers');
 
-router.get('/', index);
+router.get('/', (req, res) =>
+  res.send(`
+    <html lang="ru">
+      <head>
+        <title>Shri 2018</title>
+        <script src="/scripts/test.js"></script>
+      </head>
+      <h1>Яндекс Переговорки.<br>Тестовое задание № 1</h1>
+    </html>
+    `)
+);
 
 module.exports = router;
